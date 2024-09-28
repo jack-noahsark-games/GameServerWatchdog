4b131232d88ba9a189fd625e70916a337f217ca2
jack-noahsark-games <jack123atkinson@outlook.com>
2024-09-29 00:01:22 +0100

    Added subprocess.run functionality, this allows the process to be restarted in case of process dying. Cleaned up the return code output for user feedback. Cleaned up feedback of process running successfully to stop showing twice. Added comments for readability of code. Added some thoughts on what needs to be done to get this code working correctly. Added error handling if process did not start correctly the first time, the attempt is done again.

--- process_watchdog.py
+++ process_watchdog.py
@@ -7,21 +7,39 @@ working_directory = r"C:\Users\jack1\AppData\Local\Discord\app-1.0.9164"
 def game_server_check():
     process_running = False
     
-    for process in psutil.process_iter(['pid', 'name']):
-        if process.info['name'] == process_name:
+    for process in psutil.process_iter(['pid', 'name']): #iterates over the existin PID's + corresponding process names, name is not specified yet.
+        if process.info['name'] == process_name: #states that as it itertes, if a name of process (discord) is found, print process_ running
             print(f"{process_name} is running")
             process_running= True
             break
         
-    if process_running:
-        print(f"{process_name} is running.")
-    else:
+    if process_running == False:
         print(f"{process_name} is not running, restarting now.")
         
-        process_start = subprocess.call(discord_path, cwd=working_directory)
+        process_start = subprocess.run(discord_path, cwd=working_directory) #if process running, attempt to start program | cwd = working directory | discord_path is just the path of the .exe
+        stdout = subprocess.DEVNULL
+        stderr = subprocess.DEVNULL
         if process_start == 0:
             print("Process successfully started")
+            
+            
         else:
-            print ("Process failed with return code", process_start)
+            print ("Process failed with return code", process_start.returncode)
+            process_start = subprocess.run(discord_path, cwd=working_directory) #2nd attempt to start process            
+            if process_start == 0:
+                print("On retry, the process started")
+                
+            else:
+                print("Process did not start on two attempts and returned with error code", process_start.returncode,".","Please check if you can start this process manually.")
+
                   
 game_server_check()
+
+
+#the issue i run into here, is that with subprocess.run, it waits for the program to start which is good, and it reacts to the application closing, which is fit for purpose.
+#but when we close the script, it closes the application too. because this is how subprocess.run works.
+#however, if we use subprocess.Popen, the .exe does not close when script closes BUT there are two issues
+
+
+#1st issue, the script no longer is able to feedback to user that the process is open, when running subprocess.Popen
+#2nd issue, the script no longer reopens that application if it crashes, if using Popen, which for a watchdog script, is not working as intended.
