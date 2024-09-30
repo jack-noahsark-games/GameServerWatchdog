24605a70d1b544dce0249c214b5689defd8b8949
jack-noahsark-games <jack123atkinson@outlook.com>
2024-09-30 21:10:08 +0100

    added a while loop for attempt_process_start, instead of convoluted if statements, dramatically cleaned up the code. code is now functional

--- process_watchdog.py
+++ process_watchdog.py
@@ -13,27 +13,31 @@ def is_process_running():
             return True
         
     return False #we put return False here because if we put it as else: false, it only checks one process in the for loop and terminates early
-        
-def attempt_process_start():      
-    
-        print(f"{process_name} is not running, attempting to restart now.")
-        process_start = subprocess.Popen(process_path, cwd=working_directory)
-        return_code = process_start.returncode
-        time.sleep(5)
-        
-        if return_code == 0:
-            print("Process successfully started")
-            return True
-        else:
-            print("Process failed with return code", process_start.returncode)
-            process_start = subprocess.Popen(process_path, cwd=working_directory)
-            return_code = process_start.returncode
-            time.sleep(5)
 
-            if return_code != 0:
-                print(f"{process_name} failed to start on two occassions, with {return_code}, please check if you can open this process manually.")
-                return False
-            return True
+
+
+def attempt_process_start(): 
+    
+    attempts = 0
+    max_attempts = 2
+    
+    while attempts < max_attempts:
+        try:
+            print(f"{process_name} is not running, attempting to restart now.")
+            process_start = subprocess.Popen(process_path, cwd=working_directory)
+            time.sleep(10)
+            
+        
+            if is_process_running():
+                print("Process successfully started")
+                return True
+            else:
+                print(f"An error occured when attempting to open {process_name}.")
+                attempts += 1
+
+        except Exception as e:
+            print(f"Failed to start with exception code", e)
+            attempts += 1
   
 
 def process_monitor():
@@ -49,7 +53,7 @@ while True:
     schedule.run_pending()
     time.sleep(1)
     
-#cleaned up functions but still not working properly, the process is not recognising that the process is open, need to revisit
+
 
 
     
