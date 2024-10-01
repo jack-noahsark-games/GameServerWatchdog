795dbe77a5c41aa148137dcd31831a6320244888
jack-noahsark-games <jack123atkinson@outlook.com>
2024-10-01 21:50:52 +0100

    added nested dictionary and accompanying logic to support multiple processes in watchdog script

--- process_watchdog.py
+++ process_watchdog.py
@@ -5,36 +5,67 @@ import time
  
 processes = {
     "Discord.exe":{
-        "path": r"C:\Users\jack1\AppData\Local\Discord\app-1.0.9164\Discord.exe",
+        "process_path": r"C:\Users\jack1\AppData\Local\Discord\app-1.0.9164\Discord.exe",
         "working_directory": r"C:\Users\jack1\AppData\Local\Discord\app-1.0.9164"
     },
 
-    "Chrome.exe":{
-        "path": r"C:\Program Files\Google\Chrome\Application\Chrome.exe",
+    "chrome.exe":{
+        "process_path": r"C:\Program Files\Google\Chrome\Application\Chrome.exe",
         "working_directory": r"C:\Program Files\Google\Chrome\Application"
     },
     
     "waterfox.exe":{
-        "path": r"C:\Program Files\Waterfox\waterfox.exe",
+        "process_path": r"C:\Program Files\Waterfox\waterfox.exe",
         "working_directory": r"C:\Program Files\Waterfox"
     }
 }
 
-def is_process_running():
-    for process_name in processes: #this only acccesses the outer dictionary.
-        found = False
+def is_process_running(process_name):
         for process in psutil.process_iter(['pid', 'name']):
             if process.info['name']== process_name:
-                print(f"{process_name} is running, checking again in 30 seconds.")
-                found = True
-                break
-            if not found:
-                print(f"{process_name} is not running.")
+                print(f"{process_name} is running, checking again in 30 seconds.")                
+                return True
+        print(f"{process_name} is not running.")        
+        return False
+
+def attempt_process_start():
+    
+    max_attempts = 3
+    
+    for process_name, details in processes.items():
+        attempts = 0
+        while attempts < max_attempts:
+            try:
+                process_path = details["process_path"]
+                working_directory = details["working_directory"]
+                print(f"Attempting to start {process_name} now, waiting...")
+                process_start = subprocess.Popen(process_path, cwd=working_directory)
+                time.sleep(3)
+                
+                if is_process_running(process_name):
+                    print(f"{process_name} has successfully started on retry.")
+                    break
+                else:
+                    print(f"An error occured whilst attempting to open {process_name}, trying again.")
+                    attempts += 1
+            except Exception as e:
+                print(f"Failed to start with exception code", e)
+                if attempts >= max_attempts:
+                    print(f"Maximum attempts reached for {process_name}. Moving onto next process")
+                
+def process_monitor():
+    for process_name in processes:
+        if not is_process_running(process_name):
+            attempt_process_start()
         
-    return False #we put return False here because if we put it as else: false, it only checks one process in the for loop and terminates early
+process_monitor()     
+schedule.every(1).minutes.do(process_monitor)  
 
-is_process_running()
-
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+    
+            
 
 
 
