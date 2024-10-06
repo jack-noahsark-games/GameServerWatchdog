5fe84a420aadfecf99e29c3abfad06d7322f6d6d
jack-noahsark-games <jack123atkinson@outlook.com>
2024-10-06 22:22:38 +0100

    absolutely not working, dont know what im doing, i will figure this out

--- rcon_messager.py
+++ rcon_messager.py
@@ -1,4 +1,7 @@
 from rcon.source import Client
+import datetime
+import schedule
+message_times = ["08:00", "12:00", "18:00" , "00:00"]
 
 rcon_servers = {
     
@@ -10,7 +13,7 @@ rcon_servers = {
     "minecraft":{
         "rcon_ip" : "192.168.0.1",
         "rcon_port" : 207406,
-        "rcon_password" : "test2"
+        "rcon_password" : "test2",
     },
     "csgo":{
         "rcon_ip" : "192.168.0.1",
@@ -20,8 +23,22 @@ rcon_servers = {
     }
 }
 
-class rcon_connection():
-    with Client(rcon_ip,rcon_port,passwd = rcon_password) as client:
-        rcon_ip = rcon_servers["ark"]["rcon_port"]
-        rcon_port = rcon_servers["ark"]["rcon_port"]
-    
+time_to_restart = datetime.datetime.now()
+warning_message = "Warning: Server will restart in {time_to_restart} minutes"
+def send_warning_message(rcon_servers,details):
+    try:
+        rcon_ip = details["rcon_ip"]
+        rcon_port = details["rcon_port"]
+        rcon_password = details["rcon_password"]
+        
+        with Client(rcon_ip, rcon_port, passwd=rcon_password) as client:
+            response = client.run(f"say {warning_message}")
+        
+        
+    except Exception as e:
+        print(f"Failed to send command")
+        
+        
+        
+
+schedule.every().day.at("10:30").do(send_warning_message)    
