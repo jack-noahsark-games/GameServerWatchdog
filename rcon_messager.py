97a0f05f9b83da8221849bf26a55fbfd5792d383
jack-noahsark-games <jack123atkinson@outlook.com>
2024-10-08 21:32:22 +0100

    just playing around with the logic of the code to make sure it works, changed rcon commands to print commands for testing purposes

--- rcon_messager.py
+++ rcon_messager.py
@@ -38,41 +38,38 @@ def send_warning_message(server_details):
         rcon_port = server_details["rcon_port"]
         rcon_password = server_details["rcon_password"]
         
-        with Client(rcon_ip, rcon_port, passwd=rcon_password) as client:
-            for minutes, rcon_message in countdown_messages:
+        #with Client(rcon_ip, rcon_port, passwd=rcon_password) as client:
+        for minutes, rcon_message in countdown_messages:
                 
-                chat_command = client.run(f"say {rcon_message}.")
-                shutdown_command = client.run(f"doExit")
+            chat_feedback = f"say {rcon_message}"
+            chat_command = print(f"say {rcon_message}.")
+
                 
-                if minutes >= 5:
-                    chat_command()
-                    time.sleep(300)
-                    
-                elif minutes >= 1:
-                    chat_command()
-                    time.sleep(60)
-                    
-                elif minutes >= 0.5:
-                    chat_command()
-                    time.sleep(30)
-                    
-                elif minutes == 0:
-                    chat_command()
-                    time.sleep(10)
-                    shutdown_command()
-                    print(f"Server shutting down now...")
-                    time.sleep(30)
-                    
-                    
+            if minutes >= 5:
+                chat_feedback()
+
+                time.sleep(300)
                 
-           
+            elif minutes >= 1:
+                chat_feedback()
+                time.sleep(60)
+                
+            elif minutes >= 0.5:
+                chat_feedback()
+                time.sleep(30)
+                
+            elif minutes == 0:
+                chat_feedback()
+                time.sleep(10)
+                print(f"Server shutting down now...")
+                time.sleep(30)
+                
     except Exception as e:
-        print(f"Failed to send command")
-        
+        print(f"failed with {e}")        
         
 
     
-
+send_warning_message(rcon_servers["minecraft"])
 schedule.every().day.at("06:00").do(lambda: send_warning_message(rcon_servers["minecraft"]))
 schedule.every().day.at("12:00").do(lambda: send_warning_message(rcon_servers["minecraft"]))
 
