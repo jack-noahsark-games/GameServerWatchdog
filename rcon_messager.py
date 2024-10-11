790132536ece38309c0712bb41b24084b721a8d3
jack-noahsark-games <jack123atkinson@outlook.com>
2024-10-11 22:35:35 +0100

    slowly implementing threading, not functional yet, need to sort rest of the sleep/abort logic

--- rcon_messager.py
+++ rcon_messager.py
@@ -24,6 +24,8 @@ rcon_servers = {
     }
 }
 
+
+
 countdown_messages = [ #dictionary for the countdown messages, purpose - iterating through them
         (15, "Restarting server in 15 minutes."),
         (10, "Restarting server in 10 minutes."),
@@ -33,42 +35,61 @@ countdown_messages = [ #dictionary for the countdown messages, purpose - iterati
         (0, "Restarting server now, please exit the server.")
     ]
 
+
+
+def abort_check():
+    global abort
+    
+    while True:
+        user_input = input(f"Would you like to abort this wait: y/n?")
+        
+        if user_input.lower() == "y":
+            abort = True
+            print("Aborting...")
+            break
+        elif user_input == "n":
+            print("Script will continue to wait...")
+            
+        elif user_input != "y" or "n":
+            print("Please choose a valid input: y/n ...")    
+    
+    
+
 def send_warning_message(server_details):
+    global abort
     try:
         #these variables are not relevant for this current testing purpose, but once we switch from print to rcon.client(run) they will become relevant
         rcon_ip = server_details["rcon_ip"]
         rcon_port = server_details["rcon_port"]
         rcon_password = server_details["rcon_password"]
         
+        threading.Thread(target=abort_check, daemon=True).start() #daemon threads run in the background and automatically terminate when the main progam finishes. It ensures
+        #the background thread does not block the program from exiting once all non-daemon threads (like the main thread) are done
+        
         #with Client(rcon_ip, rcon_port, passwd=rcon_password) as client:
         for minutes, rcon_message in countdown_messages:
                 
             chat_feedback = f"say {rcon_message}"
-            skip_input = ""
+            
+            if abort:
+                print("Wait aborted, moving on...")
+                break
                 
             if minutes >= 5:
+                for _ in range(5):
+                    time.sleep(60)
+                    
                 print(chat_feedback)
-                skip_input = input("Would you like to abort this wait: y/n?")
-                if skip_input == "y":
-                    print("Wait aborted, moving on...")
-                else:
-                    time.sleep(300)
+                time.sleep(300)
                 
             elif minutes >= 1:
                 print(chat_feedback)
-                skip_input = input("Would you like to abort this wait: y/n?")
-                if skip_input == "y":
-                    print("Wait aborted, moving on...")
-                else:
-                    time.sleep(60)
+                time.sleep(60)
+                    
                 
             elif minutes >= 0.5:
                 print(chat_feedback)
-                skip_input = input("Would you like to abort this wait: y/n?")
-                if skip_input == "y":
-                    print("Wait aborted, moving on...")
-                else:
-                    time.sleep(30)
+
                 
             elif minutes == 0:
                 print(chat_feedback)
@@ -80,6 +101,7 @@ def send_warning_message(server_details):
         print(f"failed with {e}")        
         
 
+    
     
 send_warning_message(rcon_servers["minecraft"])
 schedule.every().day.at("06:00").do(lambda: send_warning_message(rcon_servers["minecraft"]))
